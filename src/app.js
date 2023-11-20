a8c63bf2253b362e7c07b812897bf90d930b95f8
Rahul Dey <rahuldey832000@gmail.com>
2023-11-20 12:33:58 +0530

    feat: express server config

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { REQUEST_SIZE_LIMIT } from "./constants.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: REQUEST_SIZE_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: REQUEST_SIZE_LIMIT }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export default app;
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "youtube-backend";
+export const REQUEST_SIZE_LIMIT = "16kb";
