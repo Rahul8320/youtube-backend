39c81d0609246aaf28b1577160edd5dab4f0e14c
Rahul Dey <rahuldey832000@gmail.com>
2023-11-28 08:30:53 +0530

    modified: user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: [true, "Full Name is required"],
       lowercase: true,
@@ -62,6 +62,35 @@ userSchema.pre("save", async function (next) {
 // compare the password
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -39,6 +39,7 @@ const videoSchema = new Schema(
   { timestamps: true },
 );
 
+// mongoose aggregation pipeline
 videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
