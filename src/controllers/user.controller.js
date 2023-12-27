b1780fab88b41a9ee69707d1b58eb4931c47e7ed
Rahul Dey <rahuldey832000@gmail.com>
2023-12-27 20:37:24 +0530

    add: logout route and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -159,4 +159,29 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+// logout user
+const logoutUser = asyncHandler(async (req, res) => {
+  // remove refresh token on database
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+
+  // set cookie headers
+  const options = { httpOnly: true, secure: true };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -12,5 +17,12 @@ router.route("/register").post(
   ]),
   registerUser,
 );
+
+// login route
+router.route("/login").post(loginUser);
+
+// secured routes
+// logout route
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
